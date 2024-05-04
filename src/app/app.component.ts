165a91169ca577c0e982034ead5ebec49b6a0829
Lens Huygh <lens.huygh@gmail.com>
2024-05-04 19:03:31 +0200

    - init + final : create components, add http call for initial data and add proxy to remedy CORS when using Docker WireMock on localhost

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HousingLocation } from './housing-location';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fairhouse';
+  selectedLocation: HousingLocation | undefined;
+  url = '/api/angular/housing/housingLocations';
+  // url = 'http://localhost:8383/api/angular/housing/housingLocations';
+  housingLocationList!: HousingLocation[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<HousingLocation[]>(this.url).subscribe(
+      (response) => {
+        this.housingLocationList = response;
+        console.log('response: ', response);
+      },
+      (error) => {
+        console.error('ERR', error.message);
+      },
+    );
+  }
+
+  updateSelectedLocation(location: HousingLocation) {
+    this.selectedLocation = location;
+  }
 }
